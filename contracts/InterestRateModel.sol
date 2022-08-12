92e1fa5fbb5dd0fe79c777c14d8c80528b9bc62e
amirfaraji78 <amirhoseinfaraji784@gmail.com>
2022-08-13 03:22:48 +0430

    interface of RateModel

---
+++ contracts/InterestRateModel.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.8.10;
+
+abstract contract InterestRateModel{
+
+    bool public constant IS_INTRESTRATEMODEL = true;
+
+    function getBarrowRate(uint cash, uint barrows, uint reserves) virtual external view returns(uint);
+
+    function getSupplyRate(uint cash, uint barrows, uint reserves, uint reservesFactoryMantinssa) virtual external view returns(uint);
+
+
+
+}
