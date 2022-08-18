d2f4fe23d0af060b356fc6cf5c20336015a1f9ca
amirfaraji78 <amirhoseinfaraji784@gmail.com>
2022-08-18 18:45:13 +0430

    config data

--- contracts/core.sol
+++ contracts/core.sol
@@ -10,6 +10,7 @@ import "./dependencies/openzeppelin/Pausable.sol";
 import "./dependencies/openzeppelin/Ownable.sol";
 import "./dependencies/openzeppelin/SafeCast.sol";
 import "./dependencies/chainlink/AggregatorV3Interface.sol";
+import "./dependencies/openzeppelin/SafeERC20.sol";
 
 import "./LendConstant.sol";
 import "./libraries/VaultAccount.sol";
@@ -17,4 +18,46 @@ import "./libraries/VaultAccount.sol";
 import "./interfaces/IERC4626.sol";
 import "./interfaces/ILendWhitelist.sol";
 import "./interfaces/IRateCalculator.sol";
-import "./interfaces/ISwapper.sol"
+import "./interfaces/ISwapper.sol";
+
+abstract contract Core is LendConstant, IERC4626, ERC20, Ownable, ReentrancyGuard, Pausable{
+    // frist and frist add library 
+    using VaultAccountingLibrary for VaultAccount;
+    using SafeERC20 for IERC20;
+    using SafeCast for uint256;
+
+    string public version = "1.0.0";
+
+    //Setting set by constructor() & initialize()
+    //------------------------------------
+
+    //Asset and collateral contracts
+    IERC20 internal immutable assetContract;
+    IERC20 internal immutable collateralContract;
+    
+    //Oracle
+    address public immutable oracleMultiply;
+    address public immutable oracleDivid;
+    address public immutable oracleNormalization;
+
+    //LTV Setting
+    uint256 public immutable maxLTV;
+
+    //Liqudiation Fee;
+    uint256 public immutable cleanLiqudationFee;
+    uint256 public immutable dirtyLiqudationFee;
+
+    //Calculator APR 
+    IRateCalculater public immutable rateContract; //Linear & Variable ...?
+    bytes public rateInitCallData;
+
+
+
+
+
+
+
+
+
+
+}
